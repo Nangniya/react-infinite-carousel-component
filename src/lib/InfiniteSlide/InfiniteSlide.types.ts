83e8431749f4c35b6622fc9140c97f120fecb626
김나연 <126762806+Nangniya@users.noreply.github.com>
2025-03-25 21:26:42 +0900

    Feat: Props 옵션 추가, npm publish CI/CD 설정, 리드미 작성 (#2)
    - gap 지정 가능
    - 화살표 옵션 overlay, outside 조건부 스타일링
    - CI/CD 파이프라인 생성
    - MIT 라이센스 부여
    - README.md 작성

--- src/lib/InfiniteSlide/InfiniteSlide.types.ts
+++ src/lib/InfiniteSlide/InfiniteSlide.types.ts
@@ -6,9 +6,11 @@ interface IResponsiveSetting {
 export interface IProps {
   slidesToScroll?: number;
   children: React.ReactNode;
+  gap?: number; // 요소들 사이의 간격
   leftArrow?: React.ReactNode;
   rightArrow?: React.ReactNode;
   auto?: boolean;
   interval?: number; // auto일 때 시간 간격
   responsive?: IResponsiveSetting[]; // 반응형
+  arrowsOverlay?: boolean; // 화살표를 컨텐츠 위에 겹치게 할지 여부
 }
--- src/lib/InnerInfiniteSlide/InnerInfiniteSlide.types.ts
+++
@@ -1,9 +0,0 @@
-export interface IProps {
-  slidesToShow?: number;
-  slidesToScroll: number;
-  children: React.ReactNode;
-  leftArrow?: React.ReactNode;
-  rightArrow?: React.ReactNode;
-  auto?: boolean;
-  interval?: number; // auto일 때 시간 간격
-}
